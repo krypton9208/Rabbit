f1e5dab2478b0eafe669ee7c753192d5fdee1085
Damian Krysta <krypton9208@gmail.com>
2016-05-22 14:30:02 +0200

    implementing MongoDb

---
+++ Rabbit/Rabbit.Models/dbContext.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbit.Models
+{
+    public class dbContext
+    {
+        private MongoDatabase database { get; set; }
+
+        public dbContext()
+        {
+            var conn = "mongo://localhost";
+            var client = new MongoClient(conn);
+#pragma warning disable CS0618 // Type or member is obsolete
+            var server = client.GetServer();
+#pragma warning restore CS0618 // Type or member is obsolete
+            database = server.GetDatabase("Rabbit");
+        }
+
+        public MongoDatabase db
+        {
+            get
+            {
+                return database;
+            }
+        }
+    }
+
+}
---
+++ Rabbit/Rabbit.Service/Service.cs
@@ -0,0 +1,54 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using Rabbit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbit.Service
+{
+    public class Service<T> : IService<T>, IDisposable
+    {
+        private readonly MongoDatabase db;
+
+        public IEnumerable<T> GetAll
+        {
+            get
+            {
+                return db.GetCollection<T>("ddas").FindAll().ToBsonDocument().ToListAsync().Result;
+            }
+        }
+
+        public Service()
+        {
+            db = new dbContext().db;
+        }
+
+        public int Add(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
