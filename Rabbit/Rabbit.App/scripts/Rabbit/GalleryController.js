61d7e49e80095b325dbf6471a3643fd9de57d9c3
krypton9208 <krypton9208@gmail.com>
2016-06-15 16:53:59 +0200

    little change

---
+++ Rabbit/Rabbit.App/scripts/Rabbit/GalleryController.js
@@ -0,0 +1,7 @@
+﻿var app = angular.module('myApp', []);
+app.service('RabbitSe', function ($http) {
+    this.GetAllRabit = function () {
+        var resp = $http.get("/api/GetAllRabbit");
+        return resp;
+    };
+});
